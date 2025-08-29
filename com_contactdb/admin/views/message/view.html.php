839465c57fce7967c65f207b53bbf74324eb92e3
jernesten <jernesten+github@gmail.com>
2025-08-29 17:58:51 +0200

    new

--- com_contactdb/admin/views/message/view.html.php
+++ com_contactdb/admin/views/message/view.html.php
@@ -1,1 +1,44 @@
+<?php
+defined('_JEXEC') or die;
 
+jimport('joomla.application.component.view');
+
+class ContactdbViewMessage extends JViewLegacy
+{
+    protected $form;
+    protected $item;
+    protected $state;
+
+    public function display($tpl = null)
+    {
+        $this->form  = $this->get('Form');
+        $this->item  = $this->get('Item');
+        $this->state = $this->get('State');
+
+        // Check for errors
+        if (count($errors = $this->get('Errors'))) {
+            throw new Exception(implode("\n", $errors), 500);
+        }
+
+        $this->addToolbar();
+        parent::display($tpl);
+    }
+
+    protected function addToolbar()
+    {
+        JFactory::getApplication()->input->set('hidemainmenu', true);
+
+        $isNew = ($this->item->id == 0);
+
+        JToolbarHelper::title($isNew ? JText::_('COM_CONTACTDB_MANAGER_MESSAGE_NEW') : JText::_('COM_CONTACTDB_MANAGER_MESSAGE_EDIT'), 'contactdb');
+
+        JToolbarHelper::apply('message.apply');
+        JToolbarHelper::save('message.save');
+        
+        if (!$isNew) {
+            JToolbarHelper::save2copy('message.save2copy');
+        }
+        
+        JToolbarHelper::cancel('message.cancel', $isNew ? 'JTOOLBAR_CANCEL' : 'JTOOLBAR_CLOSE');
+    }
+}
--- com_contactdb/views/form/view.html.php
+++ com_contactdb/views/form/view.html.php
@@ -1,1 +1,31 @@
+<?php
+defined('_JEXEC') or die;
 
+class contactdbViewform extends JViewLegacy
+{
+    protected $form;
+    protected $item;
+
+    public function display($tpl = null)
+    {
+        // Obtener datos del modelo
+        $this->form = $this->get('Form');
+        $this->item = $this->get('Item');
+
+        // Verificar errores
+        if (count($errors = $this->get('Errors'))) {
+            JError::raiseError(500, implode('<br />', $errors));
+            return false;
+        }
+
+        // Preparar la vista
+        $this->prepareDocument();
+        parent::display($tpl);
+    }
+
+    protected function prepareDocument()
+    {
+        $doc = JFactory::getDocument();
+        $doc->setTitle(JText::_('Mensaje enviado Gracias'));
+    }
+}
